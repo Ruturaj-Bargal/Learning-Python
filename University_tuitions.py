0e25c29305aeb4cea77b509b418de0ad719cc13f
DESKTOP-5GQB3QA\barga <bargalruturaj9@gmail.com>
2016-12-10 16:17:24 +0530

    minor changes

--- University_tuitions.py
+++ University_tuitions.py
@@ -19,9 +19,9 @@ udacious_univs = [['Udacity',90000,0]]
 usa_univs = [ ['California Institute of Technology',2175,37704],
               ['Harvard',19627,39849],
               ['Massachusetts Institute of Technology',10566,40732],
-              ['Princeton',7802,37000],
-              ['Rice',5879,35551],
-              ['Stanford',19535,40569],
+              ['Princeton',7852,37000],
+              ['Rice',5879,36551],
+              ['Stanford',19555,40569],
               ['Yale',11701,40500]  ]
 
 def total_enrollment(p):
